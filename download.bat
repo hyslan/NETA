c6a0d72fed8e2889328619303eab89b47fa0ab9b
Hyslan Silva Cruz <hyslansilva@gmail.com>
2024-07-20 12:02:11 -0300

    Batch files

---
+++ download.bat
@@ -0,0 +1,4 @@
+@echo off
+cd "C:\rpa\NETA\nina"
+echo "Running N1NA Download..."
+npx playwright test -g "neta_downloads"
---
+++ request.bat
@@ -0,0 +1,4 @@
+@echo off
+cd "C:\rpa\NETA\nina"
+echo "Running N1NA request..."
+npx playwright test -g "neta_processos"
