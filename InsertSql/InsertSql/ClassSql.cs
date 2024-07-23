f8af0e468a1f4db896dda8259187be3436423b45
Hyslan Silva Cruz <hyslansilva@gmail.com>
2024-07-23 15:58:03 -0300

    adds C#

---
+++ InsertSql/InsertSql/ClassSql.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace ClassSql
+{
+    public class Cls_Connection
+    {
+        private static string server = @"10.66.9.46";
+        private static string dataBase = "BD_ML";
+        private static string user = "BD_ML_SERVICE";
+        private static string password = "S@besp&2024*";
+
+        public SqlConnection BD_ML()
+        {
+            string conString = $"Data Source={server};Initial Catalog={dataBase};User ID={user};Password={password};Integrated Security=False;";
+            SqlConnection cn = new SqlConnection(conString);
+            return cn;
+        }
+    }
+}
---
+++ InsertSql/InsertSql/Program.cs
@@ -0,0 +1,45 @@
+﻿using ClassSql;
+using System.Data;
+using System.Data.SqlClient;
+
+// PROCEDURE:
+namespace InsertSql
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cls_Connection conObj = new Cls_Connection();
+            using (SqlConnection cn = conObj.BD_ML())
+            {
+                cn.Open();
+                try
+                {
+                    using (SqlDataAdapter da = new SqlDataAdapter())
+                    {
+                        string nomeProcedure = @"[BD_ML].[LESTE_AD\CargaDeDados].[sp_Carga_Neta]";
+                        Console.WriteLine("Iniciando Procedure...");
+                        da.SelectCommand = new SqlCommand(nomeProcedure, cn)
+                        {
+                            CommandType = CommandType.StoredProcedure,
+                            CommandTimeout = 900 // Definindo o timeout para 180 segundos
+                        };
+                        da.SelectCommand.ExecuteNonQuery();
+                        Console.WriteLine("Executou a Procedure - Por Dentro");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Erro ao executar a procedure: " + ex.Message);
+                }
+                finally
+                {
+                    cn.Close();
+                }
+
+                Console.WriteLine($@"CONCLUÍDA A PROCEDURE!");
+            }
+        }
+
+    }
+}
